c125568fc05800281d6e80c3429e352d90deec79
tjhon <fr.jhonk@gmail.com>
2023-04-14 17:12:00 -0700

    s

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(
 	name="synthdidpy",
 	author="D2CML",
 	version="0.1",
+    packages=find_packages(),
 	keywords='causal-inference',
+    url='https://github.com/d2cml-ai/synthdid.py',
+    license="MIT",
+    description="Synthdid",
     classifiers=[
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
