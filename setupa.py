66a7eb182cf8c671b94ad7923af2d5edfe14948a
tjhon <fr.jhonk@gmail.com>
2023-04-14 17:07:55 -0700

    none

--- setupa.py
+++ setupa.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
 	name="synthdidpy",
 	author="D2CML",
-	version="0.1.0",
+	version="0.1",
 	keywords='causal-inference',
     classifiers=[
         'Intended Audience :: Developers',
